e7c7ad86a14b65af1a83f76a4abb176cd2e925db
Aditya Varshney <aditya.varshneymail@gmail.com>
2023-10-27 23:51:12 +0530

    adding the conditon winner in row 1

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 let items = document.querySelectorAll("td");
 let currentStatus = "X";
+let row1 = document.querySelectorAll("tictoe")
 
 items.forEach(function (item) {
   item.addEventListener("click", function () {
     item.innerHTML = currentStatus;
     currentStatus = currentStatus === "X" ? "O" : "X";
+    let row1Cells = row1.querySelectorAll("td");
+    let cell1 = row1Cells[0].innerHTML;
+    let cell2 = row1Cells[1].innerHTML;
+    let cell3 = row1Cells[2].innerHTML;
 
-    for (let i = 0; i < item.length; i++) {
-      if (item === "X") {
+   
+      if (cell1 === "X" && cell2 === "X" && cell3 === "X") {
         console.log("X is the winner");
       } else {
         console.log("no");
       }
-    }
+    
   });
 });
