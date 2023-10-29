30665946998bd3f13d294db3d34b463cc9574923
adityaslyf <aditya.varshneymail@gmail.com>
2023-10-30 01:55:09 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let items = document.querySelectorAll("td");
 let currentStatus = "X";
-let row1 = document.querySelectorAll("tictoe")
+ let row1 = document.querySelectorAll("tr")[0]
+let row2 = document.querySelectorAll("tr")[1];
 
 items.forEach(function (item) {
   item.addEventListener("click", function () {
@@ -10,13 +11,26 @@ items.forEach(function (item) {
     let cell1 = row1Cells[0].innerHTML;
     let cell2 = row1Cells[1].innerHTML;
     let cell3 = row1Cells[2].innerHTML;
+    
 
+
+
+    let row1Cells2 = row2.querySelectorAll("td");
+    let cellf = row1Cells2[0].innerHTML;
+    let cells = row1Cells2[1].innerHTML;
+    let cellt = row1Cells2[2].innerHTML;
    
+      if (cellf === "X" && cells === "X" && cellt === "X") {
+        console.log("X is the winner");
+      } else {
+        console.log("no");
+      }
       if (cell1 === "X" && cell2 === "X" && cell3 === "X") {
         console.log("X is the winner");
       } else {
         console.log("no");
       }
+    
     
   });
 });
